92939e9ff9b2409d6888d2987fb218044dc67b7e
Martin Villavicencio <villavicencio.martin@protonmail.com>
2019-10-11 19:38:57 +0200

    updated xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
@@ -12,8 +13,10 @@ import XMonad.Layout.SimpleFloat
 import XMonad.Config.Gnome
 import XMonad.Config.Desktop
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
 import System.IO
+import XMonad.Layout.IndependentScreens
 
 myLayout = avoidStruts (
     Tall 1 (3/100) (1/2) |||
@@ -29,9 +32,23 @@ tabConfig = defaultTheme {
     inactiveColor = "#002b36"
 }
 
-main = xmonad $ gnomeConfig 
+conf =  gnomeConfig 
     { focusedBorderColor = "#fdf6e3" 
+--    , startupHook = setWMName "LG3D"
     , normalBorderColor =  "#002b36"
---    , layoutHook = smartBorders $ layoutHook gnomeConfig
+    , workspaces = withScreens 2 ["0", "1", "2", "3", "4", "5", "6"]
     , layoutHook = smartBorders $ myLayout
-    }
+    } `additionalKeys` myKeys
+
+myKeys = 
+    [((m .|. mod1Mask, k), windows $ onCurrentScreen f i)
+    | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
+    ++
+    [((m .|. mod1Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
+
+main = xmonad conf
